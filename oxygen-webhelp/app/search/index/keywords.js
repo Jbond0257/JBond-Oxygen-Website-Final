043191ba6293ffb53a791b6c58e21da39fbabb01
Jbond0257 <145931890+Jbond0257@users.noreply.github.com>
2023-11-05 10:48:29 -0500

    Add files via upload

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,21 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Ring",p:["p1","p2","p3","p5","p13"]},{w:"Alarm",p:["p1","p2","p5","p7","p13"]},{w:"Components",p:["p1"]},{w:"Home",p:["p2"]},{w:"Setup",p:["p2"]},{w:"Setting",p:["p3","p7","p8","p9","p10","p11"]},{w:"Up",p:["p3","p6","p7","p8","p9","p10","p11"]},{w:"Your",p:["p3","p7","p10","p11"]},{w:"Account",p:["p3"]},{w:"Emergency",p:["p4"]},{w:"Contacts",p:["p4"]},{w:"(Assisted",p:["p4"]},{w:"Monitoring",p:["p4","p6"]},{w:"Only)",p:["p4"]},{w:"Modes",p:["p5"]},{w:"Set",p:["p6"]},{w:"Assisted",p:["p6"]},{w:"Devices",p:["p7"]},{w:"The",p:["p8","p9"]},{w:"Contact",p:["p8","p12"]},{w:"Sensor",p:["p8","p12"]},{w:"Range",p:["p9"]},{w:"Extender",p:["p9"]},{w:"Base",p:["p10"]},{w:"Station",p:["p10"]},{w:"Keypad",p:["p11"]},{w:"\"Tampered\"",p:["p12"]},{w:"Understanding",p:["p13"]},{w:"Device",p:["p13"]},{w:"Codes",p:["p13"]},{w:"What",p:["p14"]},{w:"is",p:["p14"]},{w:"Self",p:["p14"]},{w:"Monitoring?",p:["p14"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[3, 4, 6, 7];
+ph["p3"]=[8, 9, 10, 3, 11];
+ph["p4"]=[12, 13, 14, 15, 16];
+ph["p5"]=[3, 4, 17];
+ph["p6"]=[18, 9, 19, 15];
+ph["p7"]=[8, 9, 10, 4, 20];
+ph["p8"]=[8, 9, 21, 22, 23];
+ph["p9"]=[8, 9, 21, 24, 25];
+ph["p10"]=[8, 9, 10, 26, 27];
+ph["p12"]=[29, 22, 23];
+ph["p11"]=[8, 9, 10, 28];
+ph["p14"]=[33, 34, 35, 36];
+ph["p13"]=[30, 3, 4, 31, 32];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {14:6,11:7,2:-1,7:-1,5:-1,6:-1,10:-1,1:-1,9:7,8:7,3:-1};
